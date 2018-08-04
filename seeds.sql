21b502aeb1e0fc90ff9e1ed10d8f658d3c39cc2d
danielpruitt <35474097+danielpruitt@users.noreply.github.com>
2018-08-04 16:25:56 -0400

    seeds and schema added

---
+++ seeds.sql
@@ -0,0 +1,33 @@
+USE bamazon_db;
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VAlUES ("Galaxy S7", "Electronics", 292.00, 10);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VAlUES ("Galaxy S7 Qi Charger", "Electronics", 20.00, 10);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VAlUES ("Friends Boxset", "Movies and TV", 55.99, 100);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VAlUES ("TCL 55 Roku TV", "Electronics", 649.00, 5);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VAlUES ("Down Comforter", "Home and Kitchen", 29.00, 80);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VAlUES ("Cactus Lamp", "Home and Kitchen", 9.95, 100);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VAlUES ("Floating Shelves", "Home and Kitchen", 15.99, 35);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VAlUES ("Adidas Swift Running Shoe", "Clothing, Shoes, and Jewelry", 51.00, 10);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VAlUES ("Avengers Infinity War 3d Bluray", "Bluray", 64.99, 20);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VAlUES ("KitchenAid Stand Up Mixer", "Home and Kitchen", 219.00, 25);
+
+SELECT * FROM products;
